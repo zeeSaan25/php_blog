6f140e0c2d633698b2bef35d60d037ec182d1bdf
zeeSaan25 <128284940+zeeSaan25@users.noreply.github.com>
2023-10-02 23:40:55 +0600

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_USER", "id21342907_blog20cse045");
+define("DB_PASS", "Nayem123@");
+define("DB_NAME", "id21342907_myblog");
+define("TITLE", "Training With Live Project");
+define("KEYWORDS", "PHP Tutorials, JAVA Tutorials, Oracle Database, C# Tutorials");
+
