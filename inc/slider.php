2fbf52d4b5bf33f39c79882a2333c3923f3b897e
zeeSaan25 <128284940+zeeSaan25@users.noreply.github.com>
2023-10-02 23:46:06 +0600

    Add files via upload

---
+++ inc/slider.php
@@ -0,0 +1,9 @@
+<div class="slidersection templete clear">
+        <div id="slider">
+            <a href="#"><img src="images/slideshow/1738.jpg" alt="nature 1" title="This is slider One " /></a>
+            <a href="#"><img src="images/slideshow/slider2.jpg" alt="nature 2" title="This is slider Two " /></a>
+            <a href="#"><img src="images/slideshow/slider3.jpg" alt="nature 3" title="This is slider Three " /></a>
+            <a href="#"><img src="images/slideshow/1738.jpg" alt="nature 4" title="This is slider Four " /></a>
+        </div>
+
+</div>
